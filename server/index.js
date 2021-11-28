eb1e00062efe37589ba90c8cf096e8fe16b59714
InHuman66 <olexandr.kk@gmail.com>
2021-11-29 00:08:28 +0200

    set crud for devices controller

--- server/index.js
+++ server/index.js
@@ -4,13 +4,17 @@ const sequelize = require('./db.js')
 const  models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
+const fileUpload = require('express-fileupload')
 const errorHandler= require('./middleware/ErrorHandlongMiddleware')
+const path = require('path')
 
 const  PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 //use in the last app.use
