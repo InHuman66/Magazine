1a73f248c087ad5b0815231c9aa94dc04d532ecd
InHuman66 <olexandr.kk@gmail.com>
2021-11-28 15:09:53 +0200

    set controllers set universal error handler set work width DB start do CRUD

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ const express = require('express')
 const sequelize = require('./db.js')
 const  models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
+const errorHandler= require('./middleware/ErrorHandlongMiddleware')
 
 const  PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
+
+//use in the last app.use
+app.use(errorHandler)
 
 app.get("/", (req, res)=>{
     res.status(200).json({message: 'Work'})
